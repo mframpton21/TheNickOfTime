ade9c89a4ccae27860032b16501c6a5dabf3b977
mframpton21 <mike521@gmail.com>
2015-03-05 13:01:54 -0700

    More changes to add in the angular-timer library

--- scripts/controllers/trackController.js
+++ scripts/controllers/trackController.js
@@ -4,32 +4,60 @@ app.controller('trackController', function ($scope, trackRef) {
 
 	////////////////////////////////////////////////////////////////
 	$scope.activities = trackRef.$asArray();
-  $scope.timerRunning = true;
+  $scope.timerRunning = false;
 
 	////////////////////////////////////////////////////////////////
 	$scope.activities.$loaded().then(function (activities) {
     console.log("Act: ", activities);
   });
 
-  $scope.startTimer = function (){
+  $scope.startTimer = function (itemObj){
 
     $scope.$broadcast('timer-start');
     $scope.timerRunning = true;
+    $scope.timerStart = true;
+    $scope.startItem = itemObj;
   };
 
-  $scope.stopTimer = function (){
+  $scope.stopTimer = function (itemObj){
 
     $scope.$broadcast('timer-stop');
     $scope.timerRunning = false;
+    $scope.timerStart = false;
+    $scope.stopItem = itemObj;
   };
 
   $scope.$on('timer-stopped', function (event, data){
+    $scope.elapseTime = data;
     console.log('Timer Stopped - data = ', data);
   });
 
-  $scope.startTracker = function(obj) {
+  $scope.timeTracker = function(itemObj) {
 
-    alert("Tracking: " + obj.$value + ":" + obj.$id);
+    console.log("timerRunning: ", $scope.timerRunning);
+
+    if ($scope.timerRunning === false) {
+      $scope.startTimer(itemObj);    
+    } else if (itemObj === $scope.startItem) {
+      $scope.stopTimer(itemObj);
+      //Write itemObj to firebase via service
+
+      console.log("Current: ", itemObj);
+      console.log("Running: ", $scope.startItem);
+      //$scope.stopTimer(itemObj);
+    } else {
+      //Stop the timer
+      $scope.stopTimer(itemObj);
+      //Grab the data object
+      console.log("elapseTime: ", $scope.elapseTime);
+      //Write $scope.startItem to firebase via service
+      
+      //Start the timer
+      $scope.startTimer(itemObj);
+
+    }
+
+    //alert("Tracking: " + obj.$value + ":" + obj.$id);
   };
 
 	////////////////////////////////////////////////////////////////
