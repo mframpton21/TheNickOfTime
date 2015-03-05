fa41e8a5fbe1d7e22523d51bc3eb6522997c518f
mframpton21 <mikef521@gmail.com>
2015-03-05 15:22:36 -0700

    Added more tracking to the tracing controller and removed timer element from track.html

--- scripts/controllers/trackController.js
+++ scripts/controllers/trackController.js
@@ -5,10 +5,13 @@ app.controller('trackController', function ($scope, trackRef) {
 	////////////////////////////////////////////////////////////////
 	$scope.activities = trackRef.$asArray();
   $scope.timerRunning = false;
+  $scope.timerStart = false;
+  $scope.startItem = {};
+  $scope.stopItem = {};
 
 	////////////////////////////////////////////////////////////////
 	$scope.activities.$loaded().then(function (activities) {
-    console.log("Act: ", activities);
+    //console.log("Act: ", activities);
   });
 
   $scope.startTimer = function (itemObj){
@@ -27,6 +30,22 @@ app.controller('trackController', function ($scope, trackRef) {
     $scope.stopItem = itemObj;
   };
 
+  $scope.clearTimer = function (itemObj){
+
+    $scope.$broadcast('timer-clear');
+    $scope.timerRunning = false;
+    $scope.timerStart = false;
+    $scope.stopItem = itemObj;
+  };
+
+  $scope.resumeTimer = function (itemObj){
+
+    $scope.$broadcast('timer-resume');
+    $scope.timerRunning = true;
+    $scope.timerStart = true;
+    $scope.startItem = itemObj;
+  };
+
   $scope.$on('timer-stopped', function (event, data){
     $scope.elapseTime = data;
     console.log('Timer Stopped - data = ', data);
@@ -36,26 +55,65 @@ app.controller('trackController', function ($scope, trackRef) {
 
     console.log("timerRunning: ", $scope.timerRunning);
 
-    if ($scope.timerRunning === false) {
-      $scope.startTimer(itemObj);    
-    } else if (itemObj === $scope.startItem) {
-      $scope.stopTimer(itemObj);
-      //Write itemObj to firebase via service
-
-      console.log("Current: ", itemObj);
-      console.log("Running: ", $scope.startItem);
-      //$scope.stopTimer(itemObj);
-    } else {
-      //Stop the timer
-      $scope.stopTimer(itemObj);
-      //Grab the data object
-      console.log("elapseTime: ", $scope.elapseTime);
-      //Write $scope.startItem to firebase via service
-      
-      //Start the timer
-      $scope.startTimer(itemObj);
-
+    switch ($scope.timerRunning) {
+      case false:
+        $scope.startTimer(itemObj);
+        console.log("Tracking: ", itemObj);
+        break;
+      case true:
+        if (itemObj === $scope.startItem) {
+          $scope.stopTimer(itemObj);
+          //Write itemObj to firebase via service
+          console.log("Item stopped to write: ", itemObj);
+        } else {
+          //Stop the timer
+          $scope.stopTimer($scope.startItem);
+          console.log("Stoped Item: ", $scope.startItem);
+          //Grab the data object
+          console.log("elapseTime: ", $scope.elapseTime);
+          //Write $scope.startItem to firebase via service
+          console.log("Item to write: ", $scope.startItem);
+          //Start the timer
+          $scope.startTimer(itemObj);
+          console.log("Tracking Item: ", itemObj);
+        }
+        break;
+      default:
+        alert ("Invalid time running value.");
+        break;
     }
+
+
+
+
+
+
+
+    // if ($scope.timerRunning === false) {
+    //   if (itemObj === $scope.startItem) {
+    //     $scope.resumeTimer(itemObj);  
+    //   } else {
+    //     $scope.startTimer(itemObj);
+    //   }
+
+    // } else if (itemObj === $scope.startItem) {
+    //   $scope.clearTimer(itemObj);
+    //   //Write itemObj to firebase via service
+
+    //   console.log("Current: ", itemObj);
+    //   console.log("Running: ", $scope.startItem);
+    //   //$scope.stopTimer(itemObj);
+    // } else {
+    //   //Stop the timer
+    //   $scope.stopTimer(itemObj);
+    //   //Grab the data object
+    //   console.log("elapseTime: ", $scope.elapseTime);
+    //   //Write $scope.startItem to firebase via service
+      
+    //   //Start the timer
+    //   $scope.startTimer(itemObj);
+
+    // }
 
     //alert("Tracking: " + obj.$value + ":" + obj.$id);
   };
