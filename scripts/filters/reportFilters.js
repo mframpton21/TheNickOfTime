76d8baae4d97e9453b3da2a6c2fd98bc6dcd78e9
mframpton21 <mikef521@gmail.com>
2015-03-11 10:00:58 -0600

    Corrected report filter

--- scripts/filters/reportFilters.js
+++ scripts/filters/reportFilters.js
@@ -4,22 +4,20 @@ app.filter('filterByType', [function () {
 
   return function (report, selectedTypes) {
 
-  	var tempReport = [];
-
-  	//console.log("Filter: ", report, selectedTypes);
-
-  	angular.forEach(report, function (item) {
-  		console.log(item, selectedTypes);
-  		if (item.billable === selectedTypes.value) {
-  			console.log("In if");
-  			tempReport.push(value);
-  		} else {
-  			return report;
-  		}
-
-  	});
-
-      
-	};
+    if (report) {
+      var tempReport = [];
+    	console.log("Filter: ", report, selectedTypes);
+      angular.forEach(report, function (item) {
+  		  console.log("In forEach ", item, selectedTypes);
+  		  if (item.billable === selectedTypes.value) {
+  			  console.log("In if");
+  			  tempReport.push(item);
+          console.log("After push ", tempReport); 
+  		  } 
+      });
+ 			return tempReport;
+    }
+    return report;
+  };
 
 }]);
